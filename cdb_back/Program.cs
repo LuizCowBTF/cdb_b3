ba37f1024121eaf71ad70bfb2a8f1056928687cb
LuizCowBTF <luizcowbtf@gmail.com>
2023-11-13 18:32:50 -0300

    frontEnd AngularCli & backEnd CSharp4.7

---
+++ cdb_back/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cdb_back
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vf = 0;
+                        
+            Console.WriteLine("Informe o valor monetário aqui: ");
+            int vi  = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade de meses para aplicação: ");
+            int tempo = int.Parse(Console.ReadLine());
+
+            vf = vi * tempo;
+
+
+            Console.WriteLine(vf);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ cdb_front/src/app/app.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'cdb_b3'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('cdb_b3');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cdb_b3 app is running!');
+  });
+});
